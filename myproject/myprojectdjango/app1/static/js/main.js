6043863eaf337be9a82eaa151537f8b68320ee92
MrPayRoll <119882992+MrPayRoll@users.noreply.github.com>
2023-03-22 00:29:34 +0300

    Add files via upload

---
+++ myproject/myprojectdjango/app1/static/js/main.js
@@ -0,0 +1,17 @@
+document.getElementById('davaToday').valueAsDate = new Date();
+document.getElementById('davaToday1').valueAsDate = new Date();
+function text1(){
+    document.querySelector('#otkydaq').value = document.querySelector('.inp1').value
+ }
+ function textt1(){
+    document.querySelector('#kydaq').value = document.querySelector('.inp6').value
+ }
+ function date(){
+    document.querySelector('#Date').value = document.querySelector('#davaToday').value
+ }
+ function date1(){
+    document.querySelector('#Date1').value = document.querySelector('#davaToday1').value
+ }
+ function Passengers(){
+    document.querySelector('#Passengers1').value = document.querySelector('.inp11').value
+ }
