37de21b95bae55ccb720e64a77e24eefdb1b3bf2
MrPayRoll <119882992+MrPayRoll@users.noreply.github.com>
2023-04-29 10:35:29 +0300

    Delete myproject/myprojectdjango directory

--- myproject/myprojectdjango/app1/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-class Otkyda(models.Model):
-    name = models.CharField(max_length=10)
-    names = models.CharField(max_length=10)
-    data_tyda = models.DateField()
-    data_otyda = models.DateField()
-    pas = models.IntegerField(max_length=5)
